0b138a4d452233f0d33e7c0d55c37cf0cd5040af
Hiroki Fujioka <hiroki976@gmail.com>
2020-09-19 17:53:45 +0900

    revert

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 
 module.exports = function fizzbuzz(n) {
     let s = '';
-    if (n % 5 == 0) {
+    if (n % 3 == 0) {
         s += 'Fizz';
     }
-    if (n % 3 == 0) {
+    if (n % 5 == 0) {
         s += 'Buzz';
     }
     return s === '' ? String(n) : s;
